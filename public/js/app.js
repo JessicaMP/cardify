d8a3656a8d400843391603d21413854ee6770ac3
JessicaMP <jessica95.mp@gmail.com>
2018-02-02 00:24:43 -0500

    ruta

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,22 @@
 'use strict';
 
-$(document).ready(function () {
+var begin = function begin() {
+  $('.style-figure').addClass('col-xs-12 col-sm-6 col-lg-3');
+  $('.container-img img').addClass('img-responsive images');
+};
+
+var active = function active() {
+  $('#active').addClass('disabled');
   $('.container-img').cardify();
+  begin();
   $('figure').addClass('col-xs-12 col-sm-6 col-lg-3');
-  $('img').addClass('img-responsive images');
-});
+  $('.style-figure').removeClass('col-xs-12 col-sm-6 col-lg-3');
+};
+
+var desactive = function desactive() {
+  $('#desactive').attr('href', 'example.html');
+};
+
+$(document).ready(begin);
+$('#active').click(active);
+$('#desactive').click(desactive);
